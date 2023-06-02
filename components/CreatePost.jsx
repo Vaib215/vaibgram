eec2709f2f376cd257272cb12ccfc1da1d5dae37
Vaib215 <21bcs10561@cuchd.in>
2023-06-03 01:37:39 +0530

    Image upload setup done

--- components/CreatePost.jsx
+++ components/CreatePost.jsx
@@ -1,9 +1,11 @@
 'use client'
+import { Client, Databases, ID, Storage } from 'appwrite';
 import React, { useState } from 'react';
 import { FaImage, FaTimes } from 'react-icons/fa';
 
 const CreatePost = () => {
   const [selectedImages, setSelectedImages] = useState([]);
+  const [text, setText] = useState('');
 
   const handleImageChange = (event) => {
     const files = Array.from(event.target.files);
@@ -16,11 +18,48 @@ const CreatePost = () => {
     setSelectedImages(updatedImages);
   };
 
+
+  const handleCreatePost = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    selectedImages.forEach((image) => {
+      formData.append('images', image);
+    });
+    const client = new Client();
+    client.setEndpoint('https://cloud.appwrite.io/v1').setProject('64727e5c326384d927f5');
+    const storage = new Storage(client);
+    // upload the images to the storage by running a loop
+    selectedImages.map((image) => {
+      storage.createFile('647a498248d44e4bc3cd',ID.unique(),image).then((response) => {
+        console.log(response);
+        // console.log(`https://cloud.appwrite.io/v1/storage/buckets/647a498248d44e4bc3cd/files/${response.$id}/view?project=64727e5c326384d927f5`)
+        setText('');
+        setSelectedImages([]);
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+    // const databases = new Databases(client);
+    // const promise = await databases.createDocument('64727fdf6a5058d3b626', '64727fe97bd40741431d',
+    //   ID.unique(),
+    //   {
+    //     text,
+    //     // send the url of the image to the database
+    //     images
+    //   }
+    // );
+    // promise.then((response) => {
+    //   console.log(response);
+    // }).catch((error) => {
+    //   console.log(error);
+    // });
+  }
+
   return (
-    <div className="card relative shadow-xl mx-auto max-w-2xl compact bg-base-200">
-      <div className="card-body">
+    <div className="card shadow-xl mx-auto max-w-2xl compact bg-base-200">
+      <form className="card-body">
         <div className="form-control">
-          <textarea placeholder="Post something..." className="textarea text-xl h-24 textarea-bordered"></textarea>
+          <textarea value={text} onChange={(e) => setText(e.target.value)} placeholder="Post something..." className="textarea text-xl h-24 textarea-bordered"></textarea>
         </div>
         <div className="flex mt-2 justify-between items-center">
           <div className="form-control">
@@ -36,15 +75,15 @@ const CreatePost = () => {
               multiple
             />
           </div>
-          <button className="w-fit card-action btn btn-sm btn-primary">POST</button>
+          <button onClick={handleCreatePost} className="w-fit card-action btn btn-sm btn-primary">POST</button>
         </div>
-      </div>
+      </form>
       {selectedImages.length > 0 && (
-        <div className="card-footer absolute top-full p-4 bg-base-200 rounded-xl mt-4">
-          <ul className="flex flex-wrap gap-2">
+        <div className="card-footer top-full px-4 pb-4 bg-base-200 rounded-xl">
+          <ul className="flex flex-wrap gap-8 mx-2">
             {selectedImages.map((image, index) => (
-              <li key={index} className="flex relative items-center card bg-base-100 mx-4 mb-2">
-                <button className="absolute right-1 top-1 btn btn-error btn-circle btn-xs bg-opacity-30" onClick={() => handleRemoveImage(index)}>
+              <li key={index} className="flex relative items-center card bg-base-100">
+                <button className="absolute right-1 top-1 btn btn-error btn-circle btn-xs bg-opacity-50" onClick={() => handleRemoveImage(index)}>
                   <FaTimes size={16} />
                 </button>
                 <img src={URL.createObjectURL(image)} className='w-32 h-32 object-cover rounded-xl' alt={image.name} />
